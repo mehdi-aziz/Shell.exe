3253887e5ea29a5d4c2ea1515ce614a82f45906d
Mehdi-aziz <115144322+mehdi-aziz@users.noreply.github.com>
2022-10-14 12:41:43 +0200

    Update add.sh

--- Script/job3/add.sh
+++ Script/job3/add.sh
@@ -1,8 +1,9 @@
-#./bin/bash
+#!/bin/bash
 
-echo "Premier nombre a additioner"
-read nb1
-echo "Deuxieme nombre a additioner"
-read nb2
-sum=$(expr $nb1 + $nb2)
-echo "$sum"
+sum=$(( $1 + $2 ))
+
+echo "Sum is: $sum"
+
+
+
+
