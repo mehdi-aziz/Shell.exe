827c65a4e5ddabb2842b28b8dfa002933d132174
Mehdi-aziz <115144322+mehdi-aziz@users.noreply.github.com>
2022-10-14 12:48:16 +0200

    Update my_calculator.sh

--- Script/job6/my_calculator.sh
+++ Script/job6/my_calculator.sh
@@ -1,17 +1,24 @@
-#./bin/bash
+#!/bin/bash
 
-if [ $2 = '+' ] ; then
-	res=$(($1 + $3))
-	echo $res
-elif [ $2 = '-' ] ; then
-        res=$(($1 - $3))
-        echo $res
-elif [ $2 = 'x' ] ; then
-        res=$(($1 * $3))
-        echo $res
-elif [ $2 = '/' ] ; then
-        res=$(($1 / $3))
-        echo $res
-else
-	echo "Please enter valid operations (+,-,/,x)"
-fi
+read -p "Quelle opération voulez-vous effectuer : [A]ddition [S]oustraction [M]ultiplication [D]ivision ?" operation
+
+case $operation in
+    [Aa]|[Aa]ddition|'+')
+        read -rp 'Donnez les deux nombres à additionner : ' var1 var2
+        echo "Résultat : $(echo $var1 + $var2| bc -l)"
+    ;;
+    [Ss]|[Ss]oustraction|'-')
+        read -rp 'Donnez les deux nombres à soustraire : ' var1 var2
+        echo "Résultat : $(echo $var1 - $var2| bc -l)"
+    ;;
+    [Mm]|[Mm]ultiplication|'*')
+        read -rp 'Donnez les deux nombres à multiplier : ' var1 var2
+        echo "Résultat : $(echo $var1 \* $var2| bc -l)"
+    ;;
+    [Dd]|[Dd]ivision|'/')
+        read -rp 'Donnez les deux nombres à diviser : ' var1 var2
+        echo "Résultat : $(echo $var1 / $var2| bc -l)"
+    ;;
+    *) echo "Opération : \"$operation\" non reconnue."
+    ;;
+esac
