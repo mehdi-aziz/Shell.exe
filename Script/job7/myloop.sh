b8d526a66897e769dc09970d3e1a2a32a7e46624
Mehdi-aziz <115144322+mehdi-aziz@users.noreply.github.com>
2022-10-14 12:49:17 +0200

    Update myloop.sh

--- Script/job7/myloop.sh
+++ Script/job7/myloop.sh
@@ -1,9 +1,6 @@
-#./bin/bash
+#!/bin/bash
 
-for i in $(seq 1 $1)
+for i in 1 2 3 4 5 6 7 8 9 10
 do
-	echo "Je suis un script qui arrive a faire des boucles $i"
-
+ echo "Je suis un script qui arrive à faire une boucle $i"
 done
-
-
